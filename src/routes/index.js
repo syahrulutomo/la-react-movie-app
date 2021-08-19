007e32b525517fc91fd912a8957f024dc5841946
Syahrul Utomo <syahrul.puji@weekendinc.com>
2021-08-19 08:04:16 +0700

    code splitting

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Switch } from 'react-router-dom'
-import HomeView from '@/views/public/home'
-import Details from '@/views/public/details'
+// import HomeView from '@/views/public/home'
+// import Details from '@/views/public/details'
 import { PublicRoute } from './components'
+
+const Home = lazy(() => import('@/views/public/home'));
+const Details = lazy(() => import('@/views/public/details'));
 
 export function Routes() {
 
   return (
     <BrowserRouter basename="/">
-      <Switch >
-        <PublicRoute exact path="/" component={HomeView} />
-        <PublicRoute path="/details/:id" component={Details} />
-      </Switch>
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <Switch >
+          <PublicRoute exact path="/" component={Home} />
+          <PublicRoute path="/details/:id" component={Details} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
